0514cae4c33761c7f42a81c7906544baaa53d0f2
thijs <36861@ma-web.nl>
2023-10-16 12:00:36 +0200

    story-update

---
+++ discord-bot/shiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii.js
@@ -0,0 +1,24 @@
+
+
+
+// Require the necessary discord.js classes
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+const { token } = require('./config.json');
+
+// Create a new client instance
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+// When the client is ready, run this code (only once)
+// We use 'c' for the event parameter to keep it separate from the already defined 'client'
+client.once(Events.ClientReady, c => {
+	console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+// Log in to Discord with your client's token
+client.login(token);
+
+
+
+
+
+MTE2MDkxODIwNjA2MDQ0MTY2Mw.GUHZRf.AZWV-hiQBbnW2_etcQmVt-bFmd4kaAWctjXDfQ
